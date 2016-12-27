aaee768b0cab6f74d8932bde1bac2afd7b9bd08e
huijuan88 <huijuan_xu@student.uml.edu>
2016-12-27 14:53:57 -0500

    Update get_image.sh

--- example/data/get_image.sh
+++ example/data/get_image.sh
@@ -3,3 +3,6 @@ unzip train2014.zip
 
 wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
 unzip val2014.zip
+
+wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip
+unzip test2015.zip
